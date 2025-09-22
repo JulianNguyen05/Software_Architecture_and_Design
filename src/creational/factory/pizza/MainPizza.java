e2392cee2da3faf5f45396300163183ce9c66a4a
Julian Nguyen <huutrong.160705@gmail.com>
2025-09-22 11:16:25 +0700

    Prototype_Pattern_MainPizza

--- src/creational/factory/pizza/MainPizza.java
+++ src/creational/factory/pizza/MainPizza.java
@@ -1,4 +1,11 @@
 package creational.factory.pizza;
 
+import creational.factory.pizza.VietNam.VietNamPizzaStore;
+
 public class MainPizza {
+    public static void main(String[] args) {
+        PizzaStore ps = new VietNamPizzaStore();
+        Pizza p = ps.orderPizza(PizzaType.HAISAN);
+        System.out.println(p.toString());
+    }
 }
