14597f67cc8ad82835c24f4b0f2b2814983ff163
Julian Nguyen <huutrong.160705@gmail.com>
2025-09-29 15:43:23 +0700

    Lab_Singleton_Upgrade_DataAccessUpgrade

--- src/creational/singleton/shape/ShapeFactory.java
+++ src/creational/singleton/shape/ShapeFactory.java
@@ -2,15 +2,15 @@ package creational.singleton.shape;
 
 public class ShapeFactory {
     public static Shape createShape(ShapeType type) {
-        switch(type) {
+        switch (type) {
             case CIRCLE:
-                return Circle.getInstance();
-            case RECTANGLE:
-                return Rectangle.getInstance();
+                return new Circle("Round Brush", "A4 Paper", "Wooden Frame");
             case TRIANGLE:
-                return Triangle.getInstance();
+                return new Triangle("Triangle Brush", "A3 Paper", "Plastic Frame");
+            case RECTANGLE:
+                return new Rectangle("Flat Brush", "A2 Paper", "Metal Frame");
             default:
-                throw new IllegalArgumentException("Unknown shape type");
+                throw new IllegalArgumentException("Unknown shape type: " + type);
         }
     }
 }
