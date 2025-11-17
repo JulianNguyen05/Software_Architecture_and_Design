0a231afa1f93f49e0b34d4537b9ebfee4653f9e9
Julian Nguyen <huutrong.160705@gmail.com>
2025-11-17 09:58:59 +0700

    Lab_Singleton_Upgrade_DataAccessUpgrade

--- src/creational/state/remote/OffState.java
+++ src/creational/state/remote/OffState.java
@@ -3,7 +3,7 @@ package creational.state.remote;
 public class OffState implements IRemoteState{
     @Override
     public void handle(RemoteControl remote) {
-        System.out.println("==OFF TV==");
+        System.out.println("==OFF TV --> ON TV==");
         remote.setState(new OnState());
     }
 }
--- src/creational/state/remote/OnState.java
+++ src/creational/state/remote/OnState.java
@@ -3,7 +3,7 @@ package creational.state.remote;
 public class OnState implements IRemoteState{
     @Override
     public void handle(RemoteControl remote) {
-        System.out.println("==ON TV==");
+        System.out.println("==ON TV --> OFF TV==");
         remote.setState(new OffState());
     }
 }
