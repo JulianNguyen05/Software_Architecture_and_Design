57e4bd22ac4e7dba53e4f4a3631e80c06461ffab
Julian Nguyen <huutrong.160705@gmail.com>
2025-11-17 14:03:31 +0700

    Lab_Singleton_Upgrade_DataAccessUpgrade

--- src/behavioral/template_method/my_sinh_vien/MainMySV.java
+++ src/behavioral/template_method/my_sinh_vien/MainMySV.java
@@ -3,6 +3,9 @@ package behavioral.template_method.my_sinh_vien;
 public class MainMySV {
     public static void main(String[] args) {
         MySinhVienDB ls = new MySinhVienDB();
-        MySinhVien sv = new MySinhVien(1, "trong", "123",
+        MySinhVien sv = new MySinhVien(1, "trong", "123", "qqq");
+        MySinhVien sv1 = new MySinhVien(1, "trong", "123", "qqq");
+        MySinhVien sv2 = new MySinhVien(1, "trong", "123", "qqq");
+        MySinhVien sv3 = new MySinhVien(1, "trong", "123", "qqq");
     }
 }
